613095fc54e132710f40595322659a51331d135c
Manish Sajandasani <msajandasani@gmail.com>
2023-02-27 23:55:39 +0530

    Second Commit

---
+++ CodeFiles/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CodeFiles.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
